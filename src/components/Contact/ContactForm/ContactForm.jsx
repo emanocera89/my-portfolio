90ae7bcd6dc83116936a6be9e416501ffcb12fa3
emanocera89 <ema.nocera.89@gmail.com>
2023-04-19 10:06:43 -0300

    added contact form functionality

--- src/components/Contact/ContactForm/ContactForm.jsx
+++ src/components/Contact/ContactForm/ContactForm.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { useTranslation } from "react-i18next";
+import emailjs from '@emailjs/browser';
+import swal from 'sweetalert';
+
+const initialValues = {
+    fullName: '',
+    email: '',
+    subject: '',
+    message: ''
+}
 
 function ContactForm() {
 
-    const [values, setValues] = useState({
-        fullName: '',
-        email: '',
-        subject: '',
-        message: ''
-    });
+    const [values, setValues] = useState(initialValues);
+    const [isDisabled, setIsDisabled] = useState(false)
+
 
     const [t] = useTranslation("global");
 
@@ -22,24 +28,35 @@ function ContactForm() {
 
     function handleSubmit(event) {
         event.preventDefault();
-        // Código para enviar el formulario
+        setIsDisabled(true);
+        
+        emailjs.sendForm('service_b1nuz4c', 'template_amtzj2p', event.target, '5Tfa5Fcx_oUg4vVw8')
+        .then(response => {
+            setValues(initialValues);
+            setIsDisabled(false);
+            swal(t("contact.submit_success_title"), t("contact.submit_success_subtitle"), "success");
+        })
+        .catch(error => { 
+            swal(t("contact.submit_error_title"), t("contact.submit_error_subtitle"), "error");
+            setIsDisabled(false);
+        })
     }
 
     return (
         <form onSubmit={handleSubmit} className="contact-form">
             <label>
-                <input type="text" name="fullName" value={values.fullName} onChange={handleChange} required placeholder={t("contact.full_name")  + '*'} />
+                <input disabled={isDisabled} type="text" name="fullName" value={values.fullName} onChange={handleChange} required placeholder={t("contact.full_name")  + '*'} />
             </label>
             <label>
-                <input type="email" name="email" value={values.email} onChange={handleChange} required placeholder={t("contact.email") + '*'} />
+                <input disabled={isDisabled} type="email" name="email" value={values.email} onChange={handleChange} required placeholder={t("contact.email") + '*'} />
             </label>
             <label>
-                <input type="text" name="subject" value={values.subject} onChange={handleChange} required placeholder={t("contact.subject")  + '*'} />
+                <input disabled={isDisabled} type="text" name="subject" value={values.subject} onChange={handleChange} required placeholder={t("contact.subject")  + '*'} />
             </label>
             <label>
-                <textarea name="message" value={values.message} rows="4" onChange={handleChange} required placeholder={t("contact.message")  + '*'} />
+                <textarea disabled={isDisabled} name="message" value={values.message} rows="4" onChange={handleChange} required placeholder={t("contact.message")  + '*'} />
             </label>
-            <button type="submit">{t("contact.submit")}</button>
+            <button type="submit" disabled={isDisabled}>{isDisabled ? t("contact.sending") : t("contact.submit") }</button>
         </form>
     )
 }
